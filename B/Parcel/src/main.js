80b26f0db5a1bcc1f5e0844e7be626f81f12a040
helongfei <643756132@qq.com>
2021-06-30 11:13:48 +0800

    <feature> 热更新测试

--- B/Parcel/src/main.js
+++ B/Parcel/src/main.js
@@ -1,3 +1,9 @@
 // ./src/main.js
 import { log } from './logger'
-log('hello parcel')
+log('hello parcel去')
+// HMR API
+if (module.hot) {
+  module.hot.accept(() => {
+    console.log('HMR～')
+  })
+}
