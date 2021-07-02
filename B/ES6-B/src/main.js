495961d47321d065b1de27cd3df1b17e74c6acde
helongfei <643756132@qq.com>
2021-07-02 14:37:45 +0800

    <feature> type="module"
    测试失败

--- B/ES6-B/src/main.js
+++ B/ES6-B/src/main.js
@@ -1,14 +1,39 @@
-import * as a from '../lib/export';
-// 导入整个模块
-import * as test from "./export.js";
-// 导入单个绑定
-import { age, name, name as personName } from "./export.js";
-console.log(a);
+// import * as a from '../lib/export';
+// // 导入整个模块
+// import * as test from "./export.js";
+// // 导入默认值
+// import { age, default as add, default as lalala, default as sum, name, name as personName } from "./export.js";
+// console.log(a);
 
-console.log(name) // Peter
+// console.log(name) // Peter
 
-console.log(name, age) // Peter 25
+// console.log(name, age) // Peter 25
 
-console.log(test.name, test.age) // Peter 25
+// console.log(test.name, test.age) // Peter 25
 
-console.log(personName) // Peter
+// console.log(personName) // Peter
+
+// // 导出默认值
+// let name = "Peter";
+// export default name;
+
+// console.log(lalala) // Peter
+
+
+// // 导出默认值、非默认值
+// let name = "Peter"
+// function sum(a, b) {
+//     return a + b;
+// }
+// export default sum;
+// export { name };
+
+// console.log(sum(1, 2)) // 3
+// console.log(name) // Peter
+
+// console.log(add(1, 2)) // 3
+// console.log(name) // Peter
+
+// if (true) {
+//   export * from './export.js';
+// }
