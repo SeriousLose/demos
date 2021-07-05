a87302f918394386061cc58c314764bf3515a5f8
helongfei <643756132@qq.com>
2021-07-05 14:18:47 +0800

    Update BW-cj.js

--- B/BW-cj.js
+++ B/BW-cj.js
@@ -23,3 +23,11 @@ exports = {
 
 exports.name = "SeriousLose"
 module.exports.age = 24
+
+
+let lists = ["./index.js", "./config.js"]
+lists.forEach((url) => require(url)) // 动态导入
+
+if (lists.length) {
+    require(lists[0]) // 动态导入
+}
