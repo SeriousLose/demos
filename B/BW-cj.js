93c3765758c0321342b285bd42f7842a9f25961f
helongfei <643756132@qq.com>
2021-07-05 14:13:46 +0800

    Create BW-cj.js

---
+++ B/BW-cj.js
@@ -0,0 +1,25 @@
+// 导出一个对象
+module.exports = {
+  name: "SeriousLose",
+  age: 24,
+  sex: "male"
+}
+
+// 导出任意值
+module.exports.name = "SeriousLose"
+module.exports.sex = null
+module.exports.age = undefined
+
+exports.name = "SeriousLose"
+exports.sex = "male"
+
+
+exports.name = "SeriousLose"
+exports.sex = "male"
+exports = {
+    name: "蛙人"
+}
+
+
+exports.name = "SeriousLose"
+module.exports.age = 24
